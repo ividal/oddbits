6e3d5b637b0f3db9e8dcd0b3d92f302561529d40
Irina Vidal <irina.vidal@gmail.com>
2016-12-10 19:06:47 +0100

    test opencv 3 installation

---
+++ testOpenCV/DisplayImage.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char** argv )
+{
+    if ( argc != 2 ) {
+        printf("usage: DisplayImage.out <Image_Path>\n");
+        return -1;
+    }
+
+    cv::Mat image;
+    image = cv::imread( argv[1], 1 );
+
+    if ( !image.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
+
+	int params[3] = {0};
+	params[0] = CV_IMWRITE_JPEG_QUALITY;
+	params[1] = 80;
+	std::vector<uchar> buf;
+	bool code = cv::imencode(".jpg", image, buf, std::vector<int>(params, params+2) );
+	std::cout<<code<<std::endl;
+    cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE );
+    cv::imshow("Display Image", image);
+    cv::waitKey(0);
+
+    return 0;
+}
